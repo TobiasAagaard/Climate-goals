e2ef18df83a8105f54360526917f500b890f188d
Tobias Aagaard <Tobias909@live.dk>
2023-01-10 13:44:37 +0100

    Footer

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 // import "./App.css";
 import Header from "./componets/header/Header";
 import Nav from "./componets/nav/Nav";
+import Footer from "./componets/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Footer />
     </div>
   );
 }
