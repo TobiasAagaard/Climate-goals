f73719c21d69a215f5a8827e1f6bb6e351ed2eb2
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2023-01-11 16:01:52 +0100

    Changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,11 @@
-// import "./App.scss";
+import "./App.scss";
+
+import { Header } from "./Components/Header/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -1,10 +1,20 @@
+import style from '../Header/Header.module.scss'
+
 import Logo from '../../Images/KT-Logo.png'
+import HeroBg from '../../Images/Bg-Hero-Blue.png'
+
+
 const Header = () => {
     return(
-        <header>
-            <div>
-                <img src="" alt="" />
+        <header className={style.headerMain}>
+            <div className={style.HeorContainer}>
+                <img src={HeroBg} alt="Hero-image" />
+            </div>
+            <div className={style.LogoContainer}>
+                <img src={Logo} alt="Klima-tossen-logo" />
             </div>
         </header>
     )
 }
+
+export {Header }
