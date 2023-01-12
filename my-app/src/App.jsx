0f36ae6f377d569b7d09e5e916192b5ec9651076
Tobias Aagaard <Tobias909@live.dk>
2023-01-12 09:59:54 +0100

    Changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.scss";
 
-import { Header } from "./Components/Header/Header"
+import { Header } from "./Components/Partials/Header/Header";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ my-app/src/Components/Partials/Header/Header.jsx
@@ -0,0 +1,29 @@
+import style from "./Header.module.scss";
+
+import Logo from "../../../Assets/Images/KT-Logo.png";
+import HeroBg from "../../../Assets/Images/Bg-Hero-Blue.png";
+
+const Header = () => {
+  return (
+    <header className={style.headerMain}>
+      <div className={style.HeorContainer}>
+        <img src={HeroBg} alt="" />
+      </div>
+      <div className={style.LogoContainer}>
+        <img src={Logo} alt="Klima-tossen-logo" />
+        <article>
+          <p>
+            "Det er rigtigt, at vi hver især kun kan bidrage lidt til den
+            samlede løsning."
+          </p>
+          <p>
+            "Lige så rigtigt er det, at vi slet ikke kan nå målet, uden at hver
+            især bidrager en smule til det."
+          </p>
+        </article>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
